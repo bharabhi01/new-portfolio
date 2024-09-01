cbc579f07f639aa4de57d1102aef8c1502ac3ee8
Abhishek Bhardwaj <bhardwajabhi2701@gmail.com>
2024-09-01 19:59:42 +0530

    Added 1st experience

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -31,22 +31,53 @@ const Header = () => {
       </Segment>
       <Segment basic>
         <h3>worked as</h3>
-        <Segment basic>
+        <Segment basic className="resume-details-segment">
           <h2>Software Development Engineer, Ather Energy</h2>
           <p>
             at, <a href="https://www.atherenergy.com/">atherenergy.com</a>
           </p>
           <List as="ul">
-            <List.Item as="li">Gaining Access</List.Item>
-            <List.Item as="li">Inviting Friends</List.Item>
             <List.Item as="li">
-              Benefits
-              <List.List as="ul">
-                <List.Item as="li">Rebates</List.Item>
-                <List.Item as="li">Discounts</List.Item>
-              </List.List>
+              Architected and Developed batch deployment system for large-scale
+              OTA updates using React, Redux, Golang, and SQL/NoSQL databases,
+              streamlining fleet management.
             </List.Item>
-            <List.Item as="li">Warranty</List.Item>
+            <List.Item as="li">
+              Improved OTA deployment workflow by automating CSV file splitting
+              and batch creation, reducing manual labour and the potential for
+              human error.
+            </List.Item>
+            <List.Item as="li">
+              Developed a one-click deployment mechanism with customizable
+              scheduling, dramatically reducing manual labour and increasing
+              operational efficiency for fleet-wide OTA updates.
+            </List.Item>
+            <List.Item as="li">
+              Optimized API performance by implementing multi-level payload
+              batching, reducing response time by 87% through frontend
+              aggregation and database query optimization.
+            </List.Item>
+            <List.Item as="li">
+              Developed automated MD5 checksum generation process triggered
+              immediately after artifact upload using crypto/MD5 Golang package.
+            </List.Item>
+            <List.Item as="li">
+              Integrated checksum data into update payload, enabling downstream
+              verification by client devices.
+            </List.Item>
+            <List.Item as="li">
+              Engineered responsive UI for live-location tracking feature in
+              Ather Mobile App PWA with MapMyIndia API integration, enhancing
+              user experience through real-time, localized vehicle positioning.
+            </List.Item>
+            <List.Item as="li">
+              Integrated PostHog events into Ather Mobile App PWA for better
+              user analytics.
+            </List.Item>
+            <List.Item as="li">
+              Streamlined internal OTA tool by removing redundant pages and
+              APIs, boosting performance and user experience.
+            </List.Item>
           </List>
         </Segment>
       </Segment>
